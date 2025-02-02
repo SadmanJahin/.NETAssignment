0c1ce57ff5c965ad3648cd356771723f14932fdd
SadmanJahin <sadmanjahin115@gmail.com>
2025-02-02 07:23:42 +0600

    feat: dynamic column load

--- Client/src/app/features/users/services/user.service.ts
+++ Client/src/app/features/users/services/user.service.ts
@@ -3,6 +3,7 @@ import { ApiService } from '../../../core/providers/api/api';
 import { USER_ENDPOINTS } from '../constants/user-endpoints';
 import { Observable } from 'rxjs';
 import { UserDto } from '../models/user';
+import { PageRequest, PageResponse } from '../../../core/models/page';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,13 @@ export class UserService {
 
   getUsers(): Observable<UserDto[]> {
     return this.apiService.getForBasicApi(USER_ENDPOINTS.GET_USERS_V1);
+  }
+
+  countUsers(pageRequest: PageRequest): Observable<number> {
+    return this.apiService.postForBasicApi<number>(USER_ENDPOINTS.COUNT_USERS_V1, pageRequest);
+  }
+
+  searchUsers(pageRequest: PageRequest): Observable<PageResponse<UserDto>> {
+    return this.apiService.postForBasicApi<PageResponse<UserDto>>(USER_ENDPOINTS.SEARCH_USERS_V1, pageRequest);
   }
 }
--- Client/src/app/features/users/views/users-list/users-list.component.ts
+++ Client/src/app/features/users/views/users-list/users-list.component.ts
@@ -2,27 +2,101 @@ import { Component, inject } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { UserDto } from '../../models/user';
 import { HttpErrorResponse } from '@angular/common/http';
+import { TableModule } from 'primeng/table';
+import { CamelToTitleCasePipe } from '../../../../core/pipes/camel-to-title-case.pipe';
+import { Filter, PageRequest, PageResponse, Pagination, Sort } from '../../../../core/models/page';
+import { ButtonModule } from 'primeng/button';
+import { Paginator, PaginatorState } from 'primeng/paginator';
+import { StringUtils } from '../../../../core/models/string-utils';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-users-list',
-  imports: [],
+  imports: [TableModule, ButtonModule, Paginator, CamelToTitleCasePipe, FormsModule],
   templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.css'
 })
 export class UsersListComponent {
-  private userService = inject(UserService)
+  private userService = inject(UserService);
+  data: UserDto[] = []
+  columns: string[] = []
+  ignoredColumns: string[] = ['contact', 'role'];
+  defaultSortBy: Sort = {
+    propertyName: 'transactionDate',
+    isAscending: false
+  }
+  paginationInfo: Pagination = {
+    pageNo: 1,
+    pageSize: 25,
+  }
+  filters: Filter[] | null = null;
+  totalCount: number = 0;
 
   ngOnInit(): void {
+    this.searchUsers();
   }
 
-  getUsers() {
-    this.userService.getUsers().subscribe({
-      next: (resp: UserDto[]) => {
-        console.log(resp);
+  searchUsers() {
+    this.getUsersCount();
+    this.getUsersData();
+  }
+
+  getUsersCount() {
+    const request: PageRequest = {
+      filters: this.getfiltersWithoutEmptyValues(),
+      sorts: null,
+      pagination: null
+    }
+
+    this.userService.countUsers(request).subscribe({
+      next: (count: number) => {
+        this.totalCount = count;
       },
       error: (err: HttpErrorResponse) => {
 
       }
     })
+  }
+
+  getUsersData() {
+    const request: PageRequest = {
+      filters: this.getfiltersWithoutEmptyValues(),
+      sorts: null,
+      pagination: this.paginationInfo
+    }
+
+    this.userService.searchUsers(request).subscribe({
+      next: (resp: PageResponse<UserDto>) => {
+        if (resp.listResponseData && resp.listResponseData?.length > 0) {
+          this.columns = Object.keys(resp.listResponseData[0]).filter(item => !this.ignoredColumns.some(igCol => igCol == item));
+          this.setFilters();
+          this.data = resp.listResponseData;
+        }
+      },
+      error: (err: HttpErrorResponse) => {
+
+      }
+    })
+  }
+
+  setFilters(): void {
+    this.filters = [];
+    this.columns.forEach(element => {
+      const filter: Filter = {
+        propertyName: element,
+        value: StringUtils.Empty
+      }
+      this.filters?.push(filter)
+    });
+  }
+
+  getfiltersWithoutEmptyValues(): Filter[] {
+    return this.filters?.filter(item => !(item.value != null && item.value == StringUtils.Empty)) ?? []
+  }
+
+  pageChange(data: PaginatorState): void {
+    this.paginationInfo.pageNo = (data.page || 0) + 1;
+    this.paginationInfo.pageSize = data.rows || 25;
+    this.getUsersData();
   }
 }
